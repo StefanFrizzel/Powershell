d27b1c0639433798a6b4eea7783f8a8319edabdd
Stefan Frizzel <1970264+StefanFrizzel@users.noreply.github.com>
2021-02-14 14:01:55 +0000

    Add files via upload

---
+++ Block sign in - multiple users.ps1
@@ -0,0 +1,26 @@
+﻿Install-Module -Name AzureAD
+
+Connect-AzureAD
+
+$userlist = "\\path\to\listOfUsers.txt"
+
+#block each user
+foreach($line in Get-Content $userlist)
+{ 
+
+   
+    $user = Get-AzureADUser -ObjectID $line
+    Set-AzureADUser -ObjectId $line -AccountEnabled 0
+    
+}
+
+
+#check status for each user
+foreach($line in Get-Content $userlist)
+{ 
+   
+    $user = Get-AzureADUser -ObjectID $line
+    Get-AzureADUser -ObjectID $user.ObjectId | Select DisplayName,UserPrincipalName,AccountEnabled
+    
+}
+
