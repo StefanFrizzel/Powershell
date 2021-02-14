49dbb8a924116f6587983446c9ff6a122a9aed08
Stefan Frizzel <1970264+StefanFrizzel@users.noreply.github.com>
2021-02-14 14:30:21 +0000

    Add files via upload

---
+++ M365/Inbox Rules for All Mailboxes.ps1
@@ -0,0 +1,17 @@
+﻿
+$Credential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+
+#Output file declaration
+$ExportCSV=".\InboxRulesReport_$((Get-Date -format yyyy-MMM-dd-ddd` hh-mm` tt).ToString()).csv"
+
+
+#actual task
+$users = (get-mailbox -resultsize unlimited).UserPrincipalName
+foreach ($user in $users)
+{
+	Get-InboxRule -Mailbox $user | Select-Object MailboxOwnerID,Name,Description,Enabled,RedirectTo, MoveToFolder,ForwardTo | Export-CSV -path $ExportCSV -NoTypeInformation -Append
+}
