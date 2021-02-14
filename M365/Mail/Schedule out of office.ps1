b0f7c3354e005e976213722c2ceb9060b835af9b
Stefan Frizzel <1970264+StefanFrizzel@users.noreply.github.com>
2021-02-14 14:35:02 +0000

    Add files via upload

---
+++ M365/Mail/Schedule out of office.ps1
@@ -0,0 +1,37 @@
+﻿#first time running
+
+Install-Module ExchangeOnlineManagement
+
+
+#run from here
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline
+
+
+#params
+$user = "john.doe@companyabc.co.uk"
+
+# enable auto reply params
+$start = "12/29/2020 15:11:00"
+$finish = "12/29/2020 15:15:00"
+$internalMessage = "auto-reply message for employees"
+$externalMessage = "auto-reply message for everyone else"
+
+
+# disable auto reply
+Set-MailboxAutoReplyConfiguration -Identity $user -AutoReplyState Disabled
+
+# enable auto reply
+Set-MailboxAutoReplyConfiguration -Identity $user -AutoReplyState Scheduled -StartTime $start -EndTime $finish -InternalMessage $internalMessage -ExternalMessage $externalMessage
+
+
+
+
+
+
+
+
+
+
+
