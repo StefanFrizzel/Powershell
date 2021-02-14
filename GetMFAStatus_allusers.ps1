cb2299b14c1bc15994a62892362b2082dcc6a9e6
Stefan Frizzel <1970264+StefanFrizzel@users.noreply.github.com>
2021-02-14 14:05:44 +0000

    Delete GetMFAStatus_allusers.ps1

--- GetMFAStatus_allusers.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-Connect-MsolService;
-
-$exportpath = "c:\temp\export.csv"
-Get-MsolUser -All | Select-Object @{N='UserPrincipalName';E={$_.UserPrincipalName}},
-
-@{N='MFA Status';E={if ($_.StrongAuthenticationRequirements.State){$_.StrongAuthenticationRequirements.State} else {"Disabled"}}},
-
-@{N='MFA Methods';E={$_.StrongAuthenticationMethods.methodtype}} | Export-Csv -Path $exportpath -NoTypeInformation
